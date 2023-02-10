cb30e92f7578a95fee051b97addb11796638ab5e
Matias Albesa <matiasalbesa@gmail.com>
2023-02-09 21:58:36 -0300

    borrador final - estudio

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ fetch('https://randomuser.me/api/?seed=6e421f67192e4f05')
 
         
         document.getElementById('nombre').innerHTML = "Nombre: " + people[0].name.first;
-        document.getElementById('mail').innerHTML = people[0].email
-        
-        
-    })
+        document.getElementById('apellido').innerHTML = "Apellido: " + people[0].name.last;
+        document.getElementById('nacimiento').innerHTML = "Fecha de nacimiento: " + people[0].dob.date;
+        document.getElementById('edad').innerHTML = "Edad: " + people[0].dob.age;
+        document.getElementById('dni').innerHTML = "DNI: " + people[0].id.value;
+        document.getElementById('nacionalidad').innerHTML = "Nacionalidad: " + people[0].nat;
+        document.getElementById('pais').innerHTML = "País: " + people[0].location.country;
+        document.getElementById('estado').innerHTML = "Estado: " + people[0].location.state;
+        document.getElementById('ciudad').innerHTML = "Ciudad: " + people[0].location.city;
+        document.getElementById('calle').innerHTML = "Calle: " + people[0].location.street.name;
+        document.getElementById('altura').innerHTML = "Altura: " + people[0].location.street.number;
+        document.getElementById('codigo_postal').innerHTML = "Código postal: " + people[0].location.postcode;
+        document.getElementById('tel').innerHTML = "Teléfono: " + people[0].phone;
+        document.getElementById('cel').innerHTML = "Celular: " + people[0].cell;
+        document.getElementById('mail').innerHTML = "Email: " + people[0].email;
+
+    }) 
 
 $(document).ready(function () {
     let url = "https://randomuser.me/api/?seed=6e421f67192e4f05";
